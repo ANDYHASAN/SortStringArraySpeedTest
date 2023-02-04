bc0c6e7b30128fdece83d03e066da1ce56c69669
ANDYHASAN <dddddgm01@gmail.com>
2023-02-04 15:55:56 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Генерация массива случайных слов размером 100000 элементов. 
+        string[] words = new string[100000];
+
+        Random rnd = new Random();
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            int length = rnd.Next(3, 8);
+
+            char[] chars = new char[length];
+
+            for (int j = 0; j < length; j++)
+                chars[j] = (char)rnd.Next('a', 'z');
+
+            words[i] = new string(chars);
+        }
+
+        // Замер времени до сортировки. 
+        Stopwatch swBeforeSort = Stopwatch.StartNew();
+
+        // Сортировка массива случайных слов. 
+        Array.Sort(words);
+
+        // Замер времени после сортировки. 
+        swBeforeSort.Stop();
+
+        Console.WriteLine($"Время, затраченное на сортировку: {swBeforeSort.ElapsedMilliseconds} ms");
+
+        Console.ReadKey();
+    }
+}
